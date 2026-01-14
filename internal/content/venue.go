8171629698ddc842fd089a8f48b062de1925e282
Adam G. Kadda <hello@adamkadda.com>
2026-01-14 19:25:40 +0100

    Add Name field to Venue

--- internal/content/venue.go
+++ internal/content/venue.go
@@ -6,11 +6,16 @@ import (
 
 type Venue struct {
 	ID           int
+	Name         string
 	FullAddress  string
 	ShortAddress string
 }
 
 func (venue *Venue) Validate() error {
+	if venue.Name == "" {
+		return ErrVenueNameEmpty
+	}
+
 	if venue.FullAddress == "" {
 		return ErrVenueFullAddressEmpty
 	}
@@ -23,6 +28,7 @@ func (venue *Venue) Validate() error {
 }
 
 var (
+	ErrVenueNameEmpty         = errors.New("venue name is empty")
 	ErrVenueFullAddressEmpty  = errors.New("venue full address is empty")
 	ErrVenueShortAddressEmpty = errors.New("venue short address is empty")
 	ErrVenueProtected         = errors.New("venue protected; deletion forbidden")
