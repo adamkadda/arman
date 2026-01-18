e545825d773a839611b61d6e29609c547bfaaa81
Adam G. Kadda <hello@adamkadda.com>
2026-01-18 15:03:59 +0100

    Add sentinel errors

--- internal/content/content.go
+++ internal/content/content.go
@@ -4,5 +4,13 @@
 //
 // The content package is only concerned with the business logic of resources. It
 // does not care about whether other resources exist, and validation functions
-// should reflect this.
+// should reflect this. However, some errors are a mix between business rule and
+// persistence violations. In such cases, those errors can be defined here.
 package content
+
+import "errors"
+
+var (
+	ErrResourceNotFound   = errors.New("resource not found")
+	ErrInvariantViolation = errors.New("unexpected number of rows affected")
+)
