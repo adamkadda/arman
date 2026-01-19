be11ecc23958140e430510f0bbc94faf2b6762df
Adam G. Kadda <hello@adamkadda.com>
2026-01-19 16:39:40 +0100

    Implement PieceStore methods

--- internal/cms/store/piece.go
+++ internal/cms/store/piece.go
@@ -2,6 +2,7 @@ package store
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/adamkadda/arman/internal/cms/models"
 	"github.com/adamkadda/arman/internal/content"
@@ -17,66 +18,207 @@ func NewPieceStore(db Executor) *PieceStore {
 	}
 }
 
+type pieceRow struct {
+	pieceID         int    `db:"piece_id"`
+	piece_title     string `db:"piece_title"`
+	composerID      int    `db:"composer_id"`
+	programme_count int    `db:"programme_count"`
+}
+
+func (r *pieceRow) toPiece() content.Piece {
+	return content.Piece{
+		ID:         r.pieceID,
+		Title:      r.piece_title,
+		ComposerID: r.composerID,
+	}
+}
+
+func (r *pieceRow) toPieceWithDetails() models.PieceWithDetails {
+	return models.PieceWithDetails{
+		Piece:          r.toPiece(),
+		ProgrammeCount: r.programme_count,
+	}
+}
+
 func (s *PieceStore) Get(
 	ctx context.Context,
 	id int,
 ) (*content.Piece, error) {
-	// TODO: Prepare query
+	query := `
+	SELECT
+		piece_id,
+		piece_title,
+		composer_id
+	FROM pieces
+	WHERE piece_id = $1
+	`
 
-	// TODO: Execute query
+	pgxRows, err := s.db.Query(ctx, query, id)
+	if err != nil {
+		return nil, fmt.Errorf("query failed: %w", err)
+	}
 
-	// TODO: Return result
+	row, err := collectRow[pieceRow](pgxRows)
+	if err != nil {
+		return nil, err
+	}
 
-	return nil, nil
+	piece := row.toPiece()
+
+	return &piece, nil
 }
 
 func (s *PieceStore) GetWithDetails(
 	ctx context.Context,
 	id int,
 ) (*models.PieceWithDetails, error) {
-	// TODO: Prepare query
+	query := `
+	SELECT
+		piece_id,
+		piece_title,
+		composer_id
+	COALESCE() AS programme_count
+	FROM pieces p
+	LEFT JOIN (
+		SELECT piece_id, COUNT(*) AS programme_count
+		FROM programme_pieces
+		WHERE piece_id = $1
+		GROUP BY composer_id
+	) pp on pp.piece_id = p.piece_id
+	WHERE piece_id = $1
+	`
 
-	// TODO: Execute query
+	pgxRows, err := s.db.Query(ctx, query, id)
+	if err != nil {
+		return nil, fmt.Errorf("query failed: %w", err)
+	}
 
-	// TODO: Return result
+	row, err := collectRow[pieceRow](pgxRows)
+	if err != nil {
+		return nil, err
+	}
 
-	return nil, nil
+	piece := row.toPieceWithDetails()
+
+	return &piece, nil
 }
 
 func (s *PieceStore) ListWithDetails(
 	ctx context.Context,
 ) ([]models.PieceWithDetails, error) {
-	// TODO: Prepare query
+	query := `
+	SELECT
+		piece_id,
+		piece_title,
+		composer_id
+	COALESCE() AS programme_count
+	FROM pieces p
+	LEFT JOIN (
+		SELECT piece_id, COUNT(*) AS programme_count
+		FROM programme_pieces
+		GROUP BY composer_id
+	) pp on pp.piece_id = p.piece_id
+	ORDER BY p.piece_id
+	`
 
-	// TODO: Execute query
+	pgxRows, err := s.db.Query(ctx, query)
+	if err != nil {
+		return nil, fmt.Errorf("query failed: %w", err)
+	}
 
-	// TODO: Return result
+	rows, err := collectRows[pieceRow](pgxRows)
+	if err != nil {
+		return nil, err
+	}
 
-	return nil, nil
+	pieces := make([]models.PieceWithDetails, 0, len(rows))
+	for _, row := range rows {
+		pieces = append(pieces, row.toPieceWithDetails())
+	}
+
+	return pieces, nil
+}
+
+func (s *PieceStore) Create(
+	ctx context.Context,
+	p content.Piece,
+) (*content.Piece, error) {
+	query := `
+	INSERT INTO pieces (piece_title, composer_id)
+	VALUES ($1, $2)
+	RETURNING
+		piece_id,
+		piece_title,
+		composer_id
+	`
+
+	pgxRows, err := s.db.Query(ctx, query,
+		p.Title,
+		p.ComposerID,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("query failed: %w", err)
+	}
+
+	row, err := collectRow[pieceRow](pgxRows)
+	if err != nil {
+		return nil, err
+	}
+
+	piece := row.toPiece()
+
+	return &piece, nil
 }
 
 func (s *PieceStore) Update(
 	ctx context.Context,
 	p content.Piece,
 ) (*content.Piece, error) {
-	// TODO: Prepare query
+	query := `
+	UPDATE pieces
+	SET
+		piece_title = $1
+		composer_id = $2
+	WHERE piece_id = $3
+	RETURNING
+		piece_id,
+		piece_title,
+		composer_id
+	`
 
-	// TODO: Execute query
+	pgxRows, err := s.db.Query(ctx, query,
+		p.Title,
+		p.ComposerID,
+		p.ID,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("query failed: %w", err)
+	}
 
-	// TODO: Return result
+	row, err := collectRow[pieceRow](pgxRows)
+	if err != nil {
+		return nil, err
+	}
 
-	return nil, nil
+	piece := row.toPiece()
+
+	return &piece, nil
 }
 
 func (s *PieceStore) Delete(
 	ctx context.Context,
 	id int,
 ) error {
-	// TODO: Prepare query
+	query := `
+	DELETE
+	FROM pieces 
+	WHERE piece_id = $1
+	`
 
-	// TODO: Execute query
+	cmdTag, err := s.db.Exec(ctx, query, id)
+	if err != nil {
+		return fmt.Errorf("query failed: %w", err)
+	}
 
-	// TODO: Return result
-
-	return nil
+	return checkAffected(cmdTag)
 }
