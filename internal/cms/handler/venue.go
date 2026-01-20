1a8764d90823153da23448e74f70e8172013b84a
Adam G. Kadda <hello@adamkadda.com>
2026-01-20 19:01:26 +0100

    Fix VenueStore implementation
    Plenty of issues. First a foremost, it needed a venueRequest struct to
    decode the request body into. Some other changes include closing the
    request body and handling blocked deletions.

--- internal/cms/handler/venue.go
+++ internal/cms/handler/venue.go
@@ -27,35 +27,60 @@ func (h *VenueHandler) Register(mux *http.ServeMux) {
 	mux.HandleFunc("GET /venues/{id}", h.get)
 	mux.HandleFunc("GET /venues", h.list)
 	mux.HandleFunc("POST /venues", h.create)
-	mux.HandleFunc("PUT /venues", h.update)
+	mux.HandleFunc("PUT /venues/{id}", h.update)
 	mux.HandleFunc("DELETE /venues/{id}", h.delete)
 }
 
-type VenueResponse struct {
-	ID           int    `json:"venue_id"`
+type venueRequest struct {
+	Name         string `json:"venue_name"`
 	FullAddress  string `json:"full_address"`
 	ShortAddress string `json:"short_address"`
 }
 
-func NewVenueResponse(v *content.Venue) VenueResponse {
-	return VenueResponse{
+func (r *venueRequest) toDomain() content.Venue {
+	return content.Venue{
+		Name:         r.Name,
+		FullAddress:  r.FullAddress,
+		ShortAddress: r.ShortAddress,
+	}
+}
+
+func (r *venueRequest) toDomainWithID(id int) content.Venue {
+	return content.Venue{
+		ID:           id,
+		Name:         r.Name,
+		FullAddress:  r.FullAddress,
+		ShortAddress: r.ShortAddress,
+	}
+}
+
+type venueResponse struct {
+	ID           int    `json:"venue_id"`
+	Name         string `json:"venue_name"`
+	FullAddress  string `json:"full_address"`
+	ShortAddress string `json:"short_address"`
+}
+
+func NewVenueResponse(v *content.Venue) venueResponse {
+	return venueResponse{
 		ID:           v.ID,
+		Name:         v.Name,
 		FullAddress:  v.FullAddress,
 		ShortAddress: v.ShortAddress,
 	}
 }
 
-type VenueWithDetailsResponse struct {
+type venueWithDetailsResponse struct {
 	ID           int    `json:"venue_id"`
 	FullAddress  string `json:"full_address"`
 	ShortAddress string `json:"short_address"`
 	EventCount   int    `json:"event_count"`
 }
 
-func NewVenueWithDetailsResponse(
+func newVenueWithDetailsResponse(
 	v *models.VenueWithDetails,
-) VenueWithDetailsResponse {
-	return VenueWithDetailsResponse{
+) venueWithDetailsResponse {
+	return venueWithDetailsResponse{
 		ID:           v.Venue.ID,
 		FullAddress:  v.Venue.FullAddress,
 		ShortAddress: v.Venue.ShortAddress,
@@ -122,9 +147,9 @@ func (h *VenueHandler) list(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	response := make([]VenueWithDetailsResponse, len(venues))
+	response := make([]venueWithDetailsResponse, len(venues))
 	for i, v := range venues {
-		response[i] = NewVenueWithDetailsResponse(&v)
+		response[i] = newVenueWithDetailsResponse(&v)
 	}
 
 	respondJSON(r.Context(), w,
@@ -134,8 +159,8 @@ func (h *VenueHandler) list(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *VenueHandler) create(w http.ResponseWriter, r *http.Request) {
-	var v content.Venue
-	if err := json.NewDecoder(r.Body).Decode(&v); err != nil {
+	var req venueRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
 		logging.FromContext(r.Context()).Warn(
 			"decode body failed",
 			slog.Any("error", err),
@@ -149,8 +174,9 @@ func (h *VenueHandler) create(w http.ResponseWriter, r *http.Request) {
 		)
 		return
 	}
+	defer r.Body.Close()
 
-	venue, err := h.venueService.Create(r.Context(), v)
+	venue, err := h.venueService.Create(r.Context(), req.toDomain())
 	if err != nil {
 		if errors.Is(err, content.ErrInvalidResource) {
 			respondJSON(r.Context(), w,
@@ -179,8 +205,26 @@ func (h *VenueHandler) create(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *VenueHandler) update(w http.ResponseWriter, r *http.Request) {
-	var v content.Venue
-	if err := json.NewDecoder(r.Body).Decode(&v); err != nil {
+	idStr := r.PathValue("id")
+
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		logging.FromContext(r.Context()).Warn(
+			"invalid id in path",
+			slog.String("id", idStr),
+		)
+
+		respondJSON(r.Context(), w,
+			http.StatusBadRequest,
+			map[string]string{
+				"error": "invalid venue ID",
+			},
+		)
+		return
+	}
+
+	var req venueRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
 		logging.FromContext(r.Context()).Warn(
 			"decode body failed",
 			slog.Any("error", err),
@@ -194,8 +238,9 @@ func (h *VenueHandler) update(w http.ResponseWriter, r *http.Request) {
 		)
 		return
 	}
+	defer r.Body.Close()
 
-	venue, err := h.venueService.Update(r.Context(), v)
+	venue, err := h.venueService.Update(r.Context(), req.toDomainWithID(id))
 	if err != nil {
 		if errors.Is(err, content.ErrInvalidResource) {
 			respondJSON(r.Context(), w,
@@ -249,6 +294,16 @@ func (h *VenueHandler) delete(w http.ResponseWriter, r *http.Request) {
 				http.StatusNotFound,
 				map[string]string{
 					"error": "venue not found",
+				},
+			)
+			return
+		}
+
+		if errors.Is(err, content.ErrVenueProtected) {
+			respondJSON(r.Context(), w,
+				http.StatusForbidden,
+				map[string]string{
+					"error": "venue referenced by published events",
 				},
 			)
 			return
