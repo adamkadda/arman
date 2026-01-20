d0df1bfb0acd3c037fcfeb0cd4607989a5fb81f4
Adam G. Kadda <hello@adamkadda.com>
2026-01-20 11:25:38 +0100

    Implement ProgrammeStore and ProgrammePieceStore methods
    By far this was the most complicated set of store methods in the store
    package. The ProgrammePieceStore's Update method was particularly
    complicated for me initially. After reviewing my previous work, I came
    to a similar conclusion that running three separate queries is best.
    
    1. First a DELETE, to remove all programmes pieces of a programme
    2. Then an INSERT with UNNEST to more easily pass arrays directly
    3. Finally, a SELECT to capture and return the inserted rows
    
    I had difficulty understanding the content package's ProgrammePiece
    definition, so it would be a good idea to add some documentation there
    for the next time I (or somebody else) gets confused.
    
    Lastly, UNNEST is pretty cool. I'm glad I didn't have to dynamically
    build a query, I don't really enjoy that for some reason.

--- internal/cms/store/programme.go
+++ internal/cms/store/programme.go
@@ -2,6 +2,7 @@ package store
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/adamkadda/arman/internal/cms/models"
 	"github.com/adamkadda/arman/internal/content"
@@ -17,30 +18,85 @@ func NewProgrammeStore(db Executor) *ProgrammeStore {
 	}
 }
 
+type programmeRow struct {
+	programmeID    int    `db:"programme_id"`
+	programmeTitle string `db:"programme_title"`
+	eventCount     int    `db:"event_count"`
+}
+
+func (r *programmeRow) toProgramme() content.Programme {
+	return content.Programme{
+		ID:    r.programmeID,
+		Title: r.programmeTitle,
+	}
+}
+
+func (r *programmeRow) toProgrammeWithDetails() models.ProgrammeWithDetails {
+	return models.ProgrammeWithDetails{
+		Programme:  r.toProgramme(),
+		EventCount: r.eventCount,
+	}
+}
+
 func (s *ProgrammeStore) Get(
 	ctx context.Context,
 	id int,
 ) (*content.Programme, error) {
-	// TODO: Prepare query
+	query := `
+	SELECT
+		programme_id,
+		programme_title
+	FROM programmes
+	WHERE programme_id = $1
+	`
 
-	// TODO: Execute query
+	pgxRows, err := s.db.Query(ctx, query, id)
+	if err != nil {
+		return nil, fmt.Errorf("query failed: %w", err)
+	}
 
-	// TODO: Return result
+	row, err := collectRow[programmeRow](pgxRows)
+	if err != nil {
+		return nil, err
+	}
 
-	return nil, nil
+	programme := row.toProgramme()
+
+	return &programme, nil
 }
 
 func (s *ProgrammeStore) GetWithDetails(
 	ctx context.Context,
 	id int,
 ) (*models.ProgrammeWithDetails, error) {
-	// TODO: Prepare query
+	query := `
+	SELECT
+		programme_id,
+		programme_title
+	COALESCE(e.event_count, 0) AS event_count
+	FROM programmes p
+	LEFT JOIN (
+	SELECT programme_id, COUNT(*) AS event_count
+	FROM events
+	WHERE venue_id = $1 AND status = "published"
+	GROUP BY programme_id
+	) e ON e.programme_id = p.programme_id
+	WHERE programme_id = $1
+	`
 
-	// TODO: Execute query
+	pgxRows, err := s.db.Query(ctx, query, id)
+	if err != nil {
+		return nil, fmt.Errorf("query failed: %w", err)
+	}
 
-	// TODO: Return result
+	row, err := collectRow[programmeRow](pgxRows)
+	if err != nil {
+		return nil, err
+	}
 
-	return nil, nil
+	programme := row.toProgrammeWithDetails()
+
+	return &programme, nil
 }
 
 // ListWithDetails returns a list of programmes with additional details. This method
@@ -50,39 +106,118 @@ func (s *ProgrammeStore) GetWithDetails(
 func (s *ProgrammeStore) ListWithDetails(
 	ctx context.Context,
 ) ([]models.ProgrammeWithDetails, error) {
-	// TODO: Prepare query
+	query := `
+	SELECT
+		programme_id,
+		programme_title
+	COALESCE(e.event_count, 0) AS event_count
+	FROM programmes p
+	LEFT JOIN (
+	SELECT programme_id, COUNT(*) AS event_count
+	FROM events
+	WHERE status = "published"
+	GROUP BY programme_id
+	) e ON e.programme_id = p.programme_id
+	ORDER BY p.programme_id
+	`
 
-	// TODO: Execute query
+	pgxRows, err := s.db.Query(ctx, query)
+	if err != nil {
+		return nil, fmt.Errorf("query failed: %w", err)
+	}
 
-	// TODO: Return result
+	rows, err := collectRows[programmeRow](pgxRows)
+	if err != nil {
+		return nil, err
+	}
 
-	return nil, nil
+	programmes := make([]models.ProgrammeWithDetails, len(rows))
+	for i, row := range rows {
+		programmes[i] = row.toProgrammeWithDetails()
+	}
+
+	return programmes, nil
+}
+
+func (s *ProgrammeStore) Create(
+	ctx context.Context,
+	p content.Programme,
+) (*content.Programme, error) {
+	query := `
+	INSERT INTO programmes (
+		programme_title
+	)
+	VALUES ($1)
+	RETURNING
+		programme_id
+		programme_title
+	`
+
+	pgxRows, err := s.db.Query(ctx, query,
+		p.Title,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("query failed: %w", err)
+	}
+
+	row, err := collectRow[programmeRow](pgxRows)
+	if err != nil {
+		return nil, err
+	}
+
+	programme := row.toProgramme()
+
+	return &programme, nil
 }
 
 func (s *ProgrammeStore) Update(
 	ctx context.Context,
 	p content.Programme,
 ) (*content.Programme, error) {
-	// TODO: Prepare query
+	query := `
+	UPDATE programmes
+	SET
+		programme_title = $1
+	WHERE programme_id = $2
+	RETURNING
+		programme_id,
+		programme_title
+	`
 
-	// TODO: Execute query
+	pgxRows, err := s.db.Query(ctx, query,
+		p.Title,
+		p.ID,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("query failed: %w", err)
+	}
 
-	// TODO: Return result
+	row, err := collectRow[programmeRow](pgxRows)
+	if err != nil {
+		return nil, err
+	}
 
-	return nil, nil
+	programme := row.toProgramme()
+
+	return &programme, nil
 }
 
 func (s *ProgrammeStore) Delete(
 	ctx context.Context,
 	id int,
 ) error {
-	// TODO: Prepare query
+	query := `
+	DELETE
+	FROM programmes
+	WHERE programme_id = $1
+	`
 
-	// TODO: Execute query
+	cmdTag, err := s.db.Exec(ctx, query, id)
+	if err != nil {
+		return fmt.Errorf("query failed: %w", err)
+	}
 
-	// TODO: Return result
-
-	return nil
+	return checkAffected(cmdTag)
 }
 
 type ProgrammePieceStore struct {
@@ -95,17 +230,65 @@ func NewProgrammePieceStore(db Executor) *ProgrammePieceStore {
 	}
 }
 
+type programmePieceRow struct {
+	pieceID    int    `db:"piece_id"`
+	pieceTitle string `db:"piece_title"`
+	composerID int    `db:"composer_id"`
+	fullName   string `db:"full_name"`
+	shortName  string `db:"short_name"`
+	sequence   int    `db:"sequence"`
+}
+
+func (r *programmePieceRow) toProgrammePiece() content.ProgrammePiece {
+	return content.ProgrammePiece{
+		Piece: content.Piece{
+			ID:    r.pieceID,
+			Title: r.pieceTitle,
+		},
+		Composer: content.Composer{
+			ID:        r.composerID,
+			FullName:  r.fullName,
+			ShortName: r.shortName,
+		},
+		Sequence: r.sequence,
+	}
+}
+
 func (s *ProgrammePieceStore) ListByProgrammeID(
 	ctx context.Context,
 	id int,
 ) ([]content.ProgrammePiece, error) {
-	// TODO: Prepare query
+	query := `
+	SELECT
+	p.piece_id,
+	p.piece_title,
+	c.composer_id,
+	c.full_name,
+	c.short_name,
+	pp.sequence
+	FROM programme_pieces pp
+	JOIN pieces p ON p.piece_id = pp.piece_id
+	JOIN composers c ON c.composer_id = p.composer_id
+	WHERE pp.programme_id = $1
+	ORDER BY pp.sequence ASC
+	`
 
-	// TODO: Execute query
+	pgxRows, err := s.db.Query(ctx, query, id)
+	if err != nil {
+		return nil, fmt.Errorf("query failed: %w", err)
+	}
 
-	// TODO: Return result
+	rows, err := collectRows[programmePieceRow](pgxRows)
+	if err != nil {
+		return nil, err
+	}
 
-	return nil, nil
+	programmePieces := make([]content.ProgrammePiece, len(rows))
+	for i, row := range rows {
+		programmePieces[i] = row.toProgrammePiece()
+	}
+
+	return programmePieces, nil
 }
 
 func (s *ProgrammePieceStore) Update(
@@ -113,11 +296,81 @@ func (s *ProgrammePieceStore) Update(
 	id int,
 	ids []int,
 ) ([]content.ProgrammePiece, error) {
-	// TODO: Prepare query
+	deleteQuery := `
+	DELETE
+	FROM programme_pieces
+	WHERE programme_id = $1
+	`
+	_, err := s.db.Exec(ctx, deleteQuery, id)
+	if err != nil {
+		return nil, fmt.Errorf("delete query failed: %w", err)
+	}
 
-	// TODO: Execute query
+	if len(ids) == 0 {
+		return []content.ProgrammePiece{}, nil
+	}
 
-	// TODO: Return result
+	sequences := make([]int, len(ids))
+	for i := range ids {
+		sequences[i] = i + 1
+	}
 
-	return nil, nil
+	// UNNEST here helps me turn arrays (or columns) of data into rows.
+	// It's especially helpful here because we don't know for sure how many
+	// pieces we want to insert. I prefer this approach over dynamically building
+	// a query. Lastly, `::int[]` is a type annotation to help UNNEST
+	// typecast the variable we insert there.
+	insertQuery := `
+	INSERT INTO programme_pieces (
+		programme_id,
+		piece_id,
+		sequence
+	)
+	SELECT
+		$1,
+		piece_id,
+		sequence
+	FROM UNNEST($2::int[], $3::int[]) AS t(piece_id, sequence)
+	`
+
+	_, err = s.db.Exec(ctx, insertQuery,
+		id,
+		ids,
+		sequences,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("insert query failed: %w", err)
+	}
+
+	selectQuery := `
+	SELECT
+		p.piece_id,
+		p.piece_title,
+		c.composer_id,
+		c.full_name,
+		c.short_name,
+		pp.sequence
+	FROM programme_pieces pp
+	JOIN pieces p ON p.piece_id = pp.piece_id
+	JOIN composers c ON c.composer_id = p.composer_id
+	WHERE pp.programme_id = $1
+	ORDER BY pp.sequence ASC
+	`
+
+	pgxRows, err := s.db.Query(ctx, selectQuery, id)
+	if err != nil {
+		return nil, fmt.Errorf("insert query failed: %w", err)
+	}
+
+	rows, err := collectRows[programmePieceRow](pgxRows)
+	if err != nil {
+		return nil, err
+	}
+
+	programmePieces := make([]content.ProgrammePiece, len(rows))
+	for i, row := range rows {
+		programmePieces[i] = row.toProgrammePiece()
+	}
+
+	return programmePieces, nil
 }
